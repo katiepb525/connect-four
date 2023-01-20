aad6da0e4c3e7a197d7d787e90a39d2d09ea70ef
katie <katiepb525@gmail.com>
2023-01-20 15:27:38 -0600

    implement #win_row? in board class

--- lib/board.rb
+++ lib/board.rb
@@ -17,4 +17,16 @@ class Board
     end
     puts (1..7).to_a.join(' ')
   end
+
+  def win_row?
+    @grid.each do |row|
+      index_start = 0
+      index_end = 3
+      return true if row[index_start..index_end].all? { |e| e == 'X' }
+
+      index_start += 3
+      index_end += 3
+      return true if row[index_start..index_end].all? { |e| e == 'X' }
+    end
+  end
 end
