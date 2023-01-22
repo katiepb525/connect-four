e78c830acda9f13a97a8ca9eb9fd74907209eeec
katie <katiepb525@gmail.com>
2023-01-22 14:13:36 -0600

    cleanup using let for readability and rubocop

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,13 +9,13 @@ describe Board do
 
   describe '#win_row?' do
     context 'there are four symbols in a horiziontal consecutively' do
-      won_row = Array.new(6) { Array.new(7) { 'O' } }
-      won_row[0][0] = 'X'
-      won_row[0][1] = 'X'
-      won_row[0][2] = 'X'
-      won_row[0][3] = 'X'
+      let(:won_row) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_row[0][0] = 'X'
+        won_row[0][1] = 'X'
+        won_row[0][2] = 'X'
+        won_row[0][3] = 'X'
         board.instance_variable_set(:@grid, won_row)
       end
 
@@ -26,13 +26,13 @@ describe Board do
     end
 
     context 'at bottom right grid, there is a row match' do
-      won_row = Array.new(6) { Array.new(7) { 'O' } }
-      won_row[5][3] = 'X'
-      won_row[5][4] = 'X'
-      won_row[5][5] = 'X'
-      won_row[5][6] = 'X'
+      let(:won_row) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_row[5][3] = 'X'
+        won_row[5][4] = 'X'
+        won_row[5][5] = 'X'
+        won_row[5][6] = 'X'
         board.instance_variable_set(:@grid, won_row)
       end
 
@@ -45,13 +45,13 @@ describe Board do
 
   describe '#win_diag_left?' do
     context 'at top left grid, there is a left facing diagonal' do
-      won_diag = Array.new(6) { Array.new(7) { 'O' } }
-      won_diag[0][0] = 'X'
-      won_diag[1][1] = 'X'
-      won_diag[2][2] = 'X'
-      won_diag[3][3] = 'X'
+      let(:won_diag) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_diag[0][0] = 'X'
+        won_diag[1][1] = 'X'
+        won_diag[2][2] = 'X'
+        won_diag[3][3] = 'X'
         board.instance_variable_set(:@grid, won_diag)
       end
 
@@ -62,13 +62,14 @@ describe Board do
     end
 
     context 'at bottom right grid, there is a left facing diagonal' do
-      won_diag = Array.new(6) { Array.new(7) { 'O' } }
-      won_diag[2][3] = 'X'
-      won_diag[3][4] = 'X'
-      won_diag[4][5] = 'X'
-      won_diag[5][6] = 'X'
+      let(:won_diag) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_diag[2][3] = 'X'
+        won_diag[3][4] = 'X'
+        won_diag[4][5] = 'X'
+        won_diag[5][6] = 'X'
+
         board.instance_variable_set(:@grid, won_diag)
       end
 
@@ -81,13 +82,13 @@ describe Board do
 
   describe '#win_diag_right?' do
     context 'at top left corner of grid, there is a right facing diagonal' do
-      won_diag = Array.new(6) { Array.new(7) { 'O' } }
-      won_diag[0][3] = 'X'
-      won_diag[1][2] = 'X'
-      won_diag[2][1] = 'X'
-      won_diag[3][0] = 'X'
+      let(:won_diag) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_diag[0][3] = 'X'
+        won_diag[1][2] = 'X'
+        won_diag[2][1] = 'X'
+        won_diag[3][0] = 'X'
         board.instance_variable_set(:@grid, won_diag)
       end
 
@@ -98,13 +99,13 @@ describe Board do
     end
 
     context 'at bottom right grid, there is a right facing diagonal' do
-      won_diag = Array.new(6) { Array.new(7) { 'O' } }
-      won_diag[2][6] = 'X'
-      won_diag[3][5] = 'X'
-      won_diag[4][4] = 'X'
-      won_diag[5][3] = 'X'
+      let(:won_diag) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_diag[2][6] = 'X'
+        won_diag[3][5] = 'X'
+        won_diag[4][4] = 'X'
+        won_diag[5][3] = 'X'
         board.instance_variable_set(:@grid, won_diag)
       end
 
@@ -117,12 +118,13 @@ describe Board do
 
   describe 'win_column?' do
     context 'there is a column match in the top left corner' do
-      won_column = Array.new(6) { Array.new(7) { 'O' } }
-      won_column[0..3].each do |row|
-        row[0] = 'X'
-      end
+      let(:won_column) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_column[0..3].each do |row|
+          row[0] = 'X'
+        end
+
         board.instance_variable_set(:@grid, won_column)
       end
 
@@ -133,12 +135,13 @@ describe Board do
     end
 
     context 'there is a column match in the bottom right corner' do
-      won_column = Array.new(6) { Array.new(7) { 'O' } }
-      won_column[2..5].each do |row|
-        row[6] = 'X'
-      end
+      let(:won_column) { Array.new(6) { Array.new(7) { 'O' } } }
 
       before do
+        won_column[2..5].each do |row|
+          row[6] = 'X'
+        end
+
         board.instance_variable_set(:@grid, won_column)
       end
 
@@ -149,17 +152,22 @@ describe Board do
     end
   end
 
-  describe '#update_board' do 
+  describe '#update_board' do
     context 'on an empty board' do
-      subject(:empty_board){ described_class.new }
+      subject(:empty_board) { described_class.new }
+      let(:blank_grid) { Array.new(6) { Array.new(7) { empty_circle } } }
+      let(:updated_grid) { Array.new(6) { Array.new(7) { empty_circle } } }
+
+      before do
+        updated_grid[5][1] = 'X'
+      end
 
       it 'updates selected column with symbol' do
         valid_input = 1
-        player_symbol = 'X'
-        blank_grid = Array.new(6) { Array.new(7) { empty_circle } }
-        updated_grid = Array.new(6) { Array.new(7) { empty_circle } }
-        updated_grid[5][1] = 'X'
-        expect{empty_board.update_board(valid_input, player_symbol)}.to change{empty_board.grid}.from(blank_grid).to(updated_grid)
+        symbol = 'X'
+        expect { empty_board.update_board(valid_input, symbol) }.to change {
+                                                                      empty_board.grid
+                                                                    }.from(blank_grid).to(updated_grid)
       end
     end
 
