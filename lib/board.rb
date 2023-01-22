18025ebf6b664e836b590945cc356b4f0a74387b
katie <katiepb525@gmail.com>
2023-01-22 14:27:01 -0600

    allow param player_symbol to be passed into win methods on board (because there will be two different symbols to check per player)

--- lib/board.rb
+++ lib/board.rb
@@ -18,53 +18,53 @@ class Board
     puts (1..7).to_a.join(' ')
   end
 
-  def win_row?
+  def win_row?(player_symbol)
     grid.each do |row|
       index_start = 0
       index_end = 3
-      return true if row[index_start..index_end].all? { |e| e == 'X' }
+      return true if row[index_start..index_end].all? { |e| e == player_symbol }
 
       index_start += 3
       index_end += 3
-      return true if row[index_start..index_end].all? { |e| e == 'X' }
+      return true if row[index_start..index_end].all? { |e| e == player_symbol }
     end
   end
 
-  def win_diag_left?
+  def win_diag_left?(player_symbol)
     grid[0..2].each_with_index do |row, idx|
       diag = [grid[idx][idx], grid[idx + 1][idx + 1], grid[idx + 2][idx + 2], grid[idx + 3][idx + 3]]
-      return true if diag.all? { |e| e == "X" }
+      return true if diag.all? { |e| e == player_symbol }
     end
 
     # catch edge cases (literally)
     grid[0..2].each_with_index do |row, idx|
       diag = [grid[idx][idx + 1], grid[idx + 1][idx + 2], grid[idx + 2][idx + 3], grid[idx + 3][idx + 4]]
-      return true if diag.all? { |e| e == "X" }
+      return true if diag.all? { |e| e == player_symbol }
     end
 
     false
   end
 
-  def win_diag_right?
+  def win_diag_right?(player_symbol)
     grid[0..2].each_with_index do |row, idx|
       diag = [grid[idx][idx + 3], grid[idx + 1][idx + 2], grid[idx + 2][idx + 1], grid[idx + 3][idx]]
-      return true if diag.all? { |e| e == "X" }
+      return true if diag.all? { |e| e == player_symbol }
     end
 
     # catch edge cases (literally)
     grid[0..2].each_with_index do |row, idx|
       diag = [grid[idx][idx + 4], grid[idx + 1][idx + 3], grid[idx + 2][idx + 2], grid[idx + 3][idx + 1]]
-      return true if diag.all? { |e| e == "X" }
+      return true if diag.all? { |e| e == player_symbol }
     end
     false
   end
 
-  def win_column?
+  def win_column?(player_symbol)
     7.times do |column_idx|
       current_column = []
       grid[0..2].each_with_index do |row, row_idx|
         current_column = [grid[row_idx][column_idx], grid[row_idx + 1][column_idx], grid[row_idx + 2][column_idx], grid[row_idx + 3][column_idx]]
-        return true if current_column.all? { |e| e == 'X' }
+        return true if current_column.all? { |e| e == player_symbol }
       end
     end
     false
